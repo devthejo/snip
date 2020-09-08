d14598df5b22ac49fcde240c742ed755d0284174
Jo <idetoile@protonmail.com>
2020-09-08 11:50:38 +0200

    add checksum of code blocks

--- plugins-native/loaders/markdown/loader.go
+++ plugins-native/loaders/markdown/loader.go
@@ -1,6 +1,7 @@
 package mainNative
 
 import (
+	"strconv"
 	"strings"
 
 	cmap "github.com/orcaman/concurrent-map"
@@ -18,6 +19,20 @@ var (
 		},
 		Load: func(cfg *loader.Config) error {
 			cfg.DefaultsPlayProps = ParseMarkdownMetas(cfg)
+
+			checksums := ParseMarkdownBlocksChecksum(cfg)
+			var varsMap map[interface{}]interface{}
+			if v, ok := cfg.DefaultsPlayProps["vars"]; ok {
+				varsMap = v.(map[interface{}]interface{})
+			} else {
+				varsMap = make(map[interface{}]interface{})
+			}
+			for i, sum := range checksums {
+				key := "SNIP_SHA256_" + strconv.Itoa(i+1)
+				varsMap[key] = sum
+			}
+			cfg.DefaultsPlayProps["vars"] = varsMap
+
 			return nil
 		},
 		PostLoad: func(cfg *loader.Config) error {
