4af69efa81914369a0e9cd3c05eef42b185ae48b
Jo <idetoile@protonmail.com>
2020-10-30 18:35:17 +0100

    bugfix

--- config/config.go
+++ config/config.go
@@ -27,8 +27,8 @@ type Config struct {
 	DeploymentName string `mapstructure:"DEPLOYMENT_NAME" json:"deployment_name,omitempty"`
 
 	PlayKey      []string `mapstructure:"KEY" json:"key,omitempty"`
-	PlayKeyDeps  bool     `mapstructure:"KEY_NO_DEPS" json:"key_deps,omitempty"`
-	PlayKeyPost  bool     `mapstructure:"KEY_NO_POST" json:"key_post,omitempty"`
+	PlayKeyDeps  bool     `mapstructure:"KEY_DEPS" json:"key_deps,omitempty"`
+	PlayKeyPost  bool     `mapstructure:"KEY_POST" json:"key_post,omitempty"`
 	PlayKeyStart string   `mapstructure:"KEY_START" json:"key_start,omitempty"`
 	PlayKeyEnd   string   `mapstructure:"KEY_END" json:"key_end,omitempty"`
 	PlayResume   bool     `mapstructure:"RESUME" json:"resume,omitempty"`
